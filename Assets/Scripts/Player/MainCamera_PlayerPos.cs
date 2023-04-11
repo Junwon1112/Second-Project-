996b74ea013319a4a9206ae6ba4764eeb510e5ea
Junwon1112 <105641787+Junwon1112@users.noreply.github.com>
2023-04-11 11:41:36 +0900

    프리팹에 완성된 무기 추가, 버섯 집 프리팹 추가, 카메라 무빙 수정
    바로 사용할 수 잇는 아이템 프리팹에 추가(기존에는 코드로만 생성)
    버섯집 에셋 다운하여 맵에 추가 및 크기 변경
    메인 카메라가 이제 플레이어를 Lerp를 써서 따라다님, 플레이어 자식에서 독립 개체로 변경

---
+++ Assets/Scripts/Player/MainCamera_PlayerPos.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera_PlayerPos : MonoBehaviour
+{
+    Vector3 playerPos;
+    float lerpRate = 15.0f;
+
+    void Update()
+    {
+        playerPos = GameManager.Instance.MainPlayer.transform.position;
+        transform.position = Vector3.Lerp(transform.position, playerPos, lerpRate * Time.deltaTime);
+        transform.rotation = GameManager.Instance.MainPlayer.transform.rotation;
+    }
+}
